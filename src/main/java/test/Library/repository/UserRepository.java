3c78bed22a61a4cff3017c03cca72c0d147d012b
BilomeriaDmytro <bilomeria.dmytro@gmail.com>
2021-03-19 16:19:48 +0200

    Redundant code

--- src/main/java/test/Library/repository/UserRepository.java
+++ src/main/java/test/Library/repository/UserRepository.java
@@ -1,14 +1,8 @@
 package test.Library.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import test.Library.model.User;
 
-import java.util.List;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    @Query(value = "select * from books where (user_id is not null) and (status = 'ACTIVE')", nativeQuery = true)
-    List<User> getAllUsersWithBooks();
 
 }
